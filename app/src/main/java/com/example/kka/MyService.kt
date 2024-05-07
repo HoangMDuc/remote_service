a8b39611eaa8068b583233f99fd771b1eccd76d9
HMD <hoangduc1882002@gmail.com>
2024-05-07 17:14:33 +0700

    AIDL

--- app/src/main/java/com/example/kka/MyService.kt
+++ app/src/main/java/com/example/kka/MyService.kt
@@ -1,12 +1,9 @@
 package com.example.kka
 
 import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.os.Handler
 import android.os.IBinder
-import android.os.Message
-import android.os.Messenger
+import android.util.Log
 import android.widget.Toast
 
 private const val MSG_REGISTER = 1;
@@ -14,30 +11,36 @@ private const val MSG_STORAGE = 2;
 private const val MSG_GET = 3;
 class MyService : Service() {
 
-    private lateinit var mMessenger: Messenger
-    internal class  IncomingHandler (context : Context,
-        private val applicationContext : Context = context.applicationContext) : Handler() {
-        override fun handleMessage(msg: Message) {
+    private val binder =  object : IMyAidlInterface.Stub() {
+        override fun findFactorialService(x: Int): Int {
+            return x * x
+        }
 
-            when(msg.what) {
-                MSG_REGISTER -> Toast.makeText(applicationContext, "REGISTER!", Toast.LENGTH_SHORT).show()
-                MSG_STORAGE -> Toast.makeText(applicationContext, "STORAGE", Toast.LENGTH_SHORT).show()
-                MSG_GET -> Toast.makeText(applicationContext, "GET", Toast.LENGTH_SHORT).show()
-                else -> super.handleMessage(msg)
-            }
-        }
-        }
+    }
+//    private lateinit var mMessenger: Messenger
+//    internal class  IncomingHandler (context : Context,
+//        private val applicationContext : Context = context.applicationContext) : Handler() {
+//        override fun handleMessage(msg: Message) {
+//
+//            when(msg.what) {
+//                MSG_REGISTER -> Toast.makeText(applicationContext, "REGISTER!", Toast.LENGTH_SHORT).show()
+//                MSG_STORAGE -> Toast.makeText(applicationContext, "STORAGE", Toast.LENGTH_SHORT).show()
+//                MSG_GET -> Toast.makeText(applicationContext, "GET", Toast.LENGTH_SHORT).show()
+//                else -> super.handleMessage(msg)
+//            }
+//        }
+//        }
 
     override fun onBind(intent: Intent): IBinder {
         Toast.makeText(applicationContext, "OnBind", Toast.LENGTH_SHORT).show()
-        mMessenger = Messenger(IncomingHandler(this))
-        return mMessenger.binder
-
+//        mMessenger = Messenger(IncomingHandler(this))
+//        return mMessenger.binder
+        return binder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-
+        Log.d("Service", "Start")
         return super.onStartCommand(intent, flags, startId)
     }
 
